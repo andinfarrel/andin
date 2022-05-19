883cf76fa5b278d6ada43f30a9a4853d32644974
andinfarrel <andinfarrel@gmail.com>
2022-05-19 15:28:04 +0100

    refactored button into a component

--- apps/andin-portfolio/src/components/common/Navbar/Navbar.tsx
+++ apps/andin-portfolio/src/components/common/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import Menu  from 'public/shapes/menu.svg'
 import Close from 'public/shapes/close.svg'
 import clsx from 'clsx'
+import { Button } from 'app/components/ui'
 
 interface MenuItem {
   menu: string,
@@ -19,20 +20,20 @@ const Navbar: FC<{
   const svgfill = activeTheme === 'light' ? "white" : 'black'
   return (
     <div className="flex fixed w-full z-50">
-      <button className="ml-auto p-3 m-4 bg-black dark:bg-white dark:text-black rounded-md text-white text-sm" onClick={() => toggleOpen()}>
+      <Button onClick={() => toggleOpen()}>
         <Menu height="20px" width="20px" fill={svgfill}/>
-      </button>
+      </Button>
       { isOpen && (
         <div className="bg-[#006D77] dark:bg-[#C6E2EC] bg-opacity-80 backdrop-blur w-screen lg:w-[40vw] h-screen fixed right-0">
           <div className="flex">
-            <button className="ml-auto p-3 m-4 bg-black dark:bg-white dark:text-black rounded-md text-white text-sm" onClick={() => toggleOpen()}>
+            <Button onClick={() => toggleOpen()}>
               <Close height="20px" width="20px" fill={svgfill} className={clsx('z-20',{'hidden': !isOpen})}/>
-            </button>
+            </Button>
           </div>
           <div className="flex flex-col">
-            <button className="ml-auto p-3 m-4 bg-black dark:bg-white dark:text-black rounded-md text-white text-sm" onClick={() => activeTheme === 'light' ? setActiveTheme('dark') : setActiveTheme('light')}>
+            <Button onClick={() => activeTheme === 'light' ? setActiveTheme('dark') : setActiveTheme('light')}>
               Toggle Theme
-            </button>
+            </Button>
           </div>
         </div>
       )}
--- apps/andin-portfolio/src/components/ui/index.ts
+++ apps/andin-portfolio/src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Window } from './Window'
 export { default as Item } from './Item'
 export { default as ToggleHeading } from './ToggleHeading'
+export { default as Button } from './Button'
