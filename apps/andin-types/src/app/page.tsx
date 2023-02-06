829dc2198c12080bae54a0df60509c0f1fe1b7f1
andinfarrel <andinfarrel@gmail.com>
2023-02-06 21:58:32 +0000

    Andin Types: improved styles

--- apps/andin-types/src/app/page.tsx
+++ apps/andin-types/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 export default function Home() {
   return (
     <main className="min-h-screen text-slate-800 bg-slate-100">
-      <div className="p-8 sm:p-24">
+      <div className="p-6 mx-auto md:p-12 md:w-3/4 lg:w-2/3">
         <PageHeader />
         <BlogList />
       </div>
@@ -13,10 +15,31 @@ export default function Home() {
 
 const PageHeader: FC = () => {
   return (
-    <div className="flex flex-col p-4 space-y-4 sm:py-8">
-      <h1 className="text-4xl sm:text-7xl">
-        <span className="font-bold ">Andin&apos;s</span> 🧠 💩
+    <div
+      className="flex flex-col p-4 space-y-2 sm:space-y-4 sm:py-8"
+      typeof="button"
+    >
+      <h1 className="text-4xl lg:text-7xl">
+        <span className="font-bold">Types</span>
       </h1>
+      <div className="w-fit">
+        <Link target="_blank" href="https://www.andinfarrel.codes">
+          <div className="flex px-2 py-2 space-x-2 transition ease-in-out rounded-md shadow-inner hover:-translate-y-[0.125rem] hover:shadow-md hover:bg-slate-100 bg-slate-100 place-items-center">
+            <div className="w-10 h-10 overflow-hidden bg-white rounded-full">
+              <Image
+                src="/images/andin-weeb.jpeg"
+                alt="Andin's profile picture, credit goes to https://picrew.me/image_maker/197705"
+                width={50}
+                height={50}
+              />
+            </div>
+            <div>
+              <h4 className="font-semibold text-md">Andin&apos;s 🧠 💩</h4>
+              <h5 className="text-sm">Software engineer</h5>
+            </div>
+          </div>
+        </Link>
+      </div>
       <h3 className="text-lg italic font-bold sm:text-2xl">
         Type and machine goes brr
       </h3>
@@ -39,8 +62,8 @@ const BlogList: FC = () => {
 
 const BlogPreview: FC = () => {
   return (
-    <div className="flex flex-col p-4 space-y-1 rounded-md hover:cursor-pointer hover:bg-slate-50 bg-opacity-5 sm:space-y-2">
-      <h3 className="text-xl font-medium sm:text-4xl">Blog title</h3>
+    <div className="transition ease-in-out hover:-translate-y-[0.125rem] flex flex-col p-4 space-y-1 rounded-md hover:shadow-inner hover:cursor-pointer hover:bg-slate-50 bg-opacity-5 sm:space-y-2">
+      <h2 className="text-2xl font-semibold sm:text-4xl">Blog title</h2>
       <p className="text-sm sm:text-md line-clamp-3">
         short example of content that will get trimmed here. short example of
         content that will get trimmed here. short example of content that will
