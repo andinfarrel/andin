d7cf855c56c0102a4920ec70056fcac4b974e48d
andinfarrel <andinfarrel@gmail.com>
2023-02-21 23:29:35 +0000

    aws: explicit key env vars

--- apps/andin-types/src/services/dynamo.ts
+++ apps/andin-types/src/services/dynamo.ts
@@ -1,3 +1,9 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-export const dynamo = new DynamoDBClient({ region: "eu-west-2" });
+export const dynamo = new DynamoDBClient({
+  region: "eu-west-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
