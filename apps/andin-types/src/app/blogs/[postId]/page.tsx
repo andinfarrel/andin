eafd5cc9a2ea5ec1e240dbfbab919f829e3a98db
andinfarrel <andinfarrel@gmail.com>
2023-02-25 17:31:34 +0000

    working build and removed unnecessary dep

---
+++ apps/andin-types/src/app/blogs/[postId]/page.tsx
@@ -0,0 +1,33 @@
+import { getPost, getPosts } from "@/services/blog";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export async function generateStaticParams() {
+  const posts = await getPosts();
+
+  return (
+    posts?.map((post) => ({
+      postId: post.id,
+    })) ?? []
+  );
+}
+
+export default async function Blog({ params }: { params: { postId: string } }) {
+  const { postId } = params;
+  const blog = await getPost(postId);
+  if (!blog) redirect("/");
+
+  return (
+    <main className="flex flex-col p-8 space-y-8">
+      <div className="font-medium whitespace-pre-wrap">
+        <p>{blog.content}</p>
+      </div>
+
+      <Link href="/">
+        <p className="italic hover:underline underline-offset-4 text-slate-600">
+          ← back
+        </p>
+      </Link>
+    </main>
+  );
+}
