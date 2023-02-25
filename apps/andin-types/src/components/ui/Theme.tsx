42e774d4305915a78892a6312971e4ddfb2f6a4c
andinfarrel <andinfarrel@gmail.com>
2023-02-25 23:06:15 +0000

    styles: fancy bg

--- apps/andin-types/src/components/ui/Theme.tsx
+++ apps/andin-types/src/components/ui/Theme.tsx
@@ -2,11 +2,16 @@ import { FC, ReactNode } from "react";
 
 const Theme: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div>
-      <main className="min-h-screen dark text-slate-800 bg-slate-200 dark:text-slate-50 dark:bg-slate-800">
-        <div className="p-8 mx-auto md:p-12 md:w-3/4 lg:w-2/3">{children}</div>
-      </main>
-    </div>
+    <main className="relative flex flex-col h-full min-h-screen overflow-hidden dark text-slate-800 bg-slate-200 dark:text-slate-50 dark:bg-slate-800">
+      <div className="fixed z-10 flex w-full h-full">
+        <div className="absolute bg-purple-200 rounded-full top-[32rem] h-[32rem] w-[32rem] aspect-square mix-blend-multiply filter blur-xl animate-blob" />
+        <div className="absolute right-0 bg-red-200 rounded-full top-40 h-80 w-80 aspect-square mix-blend-multiply filter blur-xl animate-blob" />
+        <div className="absolute top-0 bg-orange-200 rounded-full h-80 w-80 aspect-square mix-blend-multiply filter blur-xl animate-pulse" />
+      </div>
+      <div className="relative z-10 p-8 mx-auto md:p-12 md:w-3/4 lg:w-2/3">
+        {children}
+      </div>
+    </main>
   );
 };
 
--- apps/andin-types/tailwind.config.js
+++ apps/andin-types/tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
         pressed: "inset 2px 2px 6px #c0c5cc, inset -2px -2px 6px #ffffff",
         bumped: "2px 2px 6px #c0c5cc, -2px -2px 6px #ffffff",
       },
+      animation: {
+        blob: "blob 6s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "trablsate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
